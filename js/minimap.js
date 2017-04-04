529b84004418be2c867f22a268c715a25cb41aaa
Cameron Dempsey <cameron.dempsey@gmail.com>
2017-04-04 21:29:31 +1000

    Add minimap
    Replace map placeholder with interactive minimap in index.html

---
+++ js/minimap.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+
+    var map = L.map('minimap', {
+        center: [-37.815, 144.965],
+        zoom: 13
+    });
+
+    L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoiY2RlbXBzZXkiLCJhIjoiY2oweDk0NG85MDA4bzJ3bzJzOGZkaGdoaCJ9.EHeZhg7cyJ5MAfpwwA4Clw', {
+        attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
+        maxZoom: 18,
+        id: 'mapbox.streets',
+        accessToken: 'pk.eyJ1IjoiY2RlbXBzZXkiLCJhIjoiY2oweDk0NG85MDA4bzJ3bzJzOGZkaGdoaCJ9.EHeZhg7cyJ5MAfpwwA4Clw'
+    }).addTo(map);
+
+});
