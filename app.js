1a3aef8f0be73f7af7eb8d2a93f0bba111ae8ab9
samchungy <samchungy@gmail.com>
2017-05-05 02:13:37 +1000

    fixed css, submit buttons

--- app.js
+++ app.js
@@ -22,12 +22,12 @@ mongoose.connect(configDB.url); // connect to our database
 // require('./config/passport')(passport); // pass passport for configuration
 
 // set up our express application
+app.use(express.static('public'));
 app.use(morgan('dev')); // log every request to the console
 app.use(cookieParser()); // read cookies (needed for auth)
 app.use(bodyParser()); // get information from html forms
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json());
-
 
 app.set('view engine', 'ejs'); // set up ejs for templating
 
