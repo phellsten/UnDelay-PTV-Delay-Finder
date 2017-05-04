7368025614de1368f935dcaa3b0a64d562ab3918
samchungy <samchungy@gmail.com>
2017-05-05 02:31:44 +1000

    fixed up routes

--- app/routes.js
+++ app/routes.js
@@ -57,7 +57,7 @@ module.exports = function(app, passport) {
 	// we will want this protected so you have to be logged in to visit
 	// we will use route middleware to verify this (the isLoggedIn function)
 	app.get('/profile', isLoggedIn, function(req, res) {
-		res.render('profile', {
+		res.render('index', {
       pageId: 'profile',
 			user : req.user // get the user out of session and pass to template
 		});
@@ -88,6 +88,7 @@ module.exports = function(app, passport) {
      });
   });
 
+  app.use('/map', map);
 };
 
 // route middleware to make sure
