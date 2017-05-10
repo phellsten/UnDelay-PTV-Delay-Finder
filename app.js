0ce1ea7f99b5c2f301c479b1f2880df0fba1da10
samchungy <samchungy@gmail.com>
2017-05-10 13:29:36 +1000

    Try to fix issue submission

--- app.js
+++ app.js
@@ -37,46 +37,6 @@ app.use(passport.initialize());
 app.use(passport.session()); // persistent login sessions
 app.use(flash()); // use connect-flash for flash messages stored in session
 
-app.post('/formsubmit', function(req, res) {
-
-    var location = req.body.location;
-    var description = req.body.description;
-    var selection = req.body.sel1;
-    var delaytype = req.body.delaytype;
-    var locationdata = req.body.locationdata;
-
-
-
-    var MongoClient = require('mongodb').MongoClient
-        , format = require('util').format;
-
-    MongoClient.connect('mongodb://127.0.0.1:27017/userdata', function(err, db) {
-        if(err) throw err;
-
-        var collection = db.collection('userdata');
-        collection.insert({
-            'location' : location,
-            'description' : description,
-            'type' : selection,
-            'DelayType' : delaytype,
-            'locationData' : locationdata,
-        }, function(err, docs) {
-            collection.count(function(err, count) {
-                console.log(format("count = %s", count));
-            });
-        });
-
-        // Locate all the entries using find
-        collection.find().toArray(function(err, results) {
-            console.dir(results);
-            // Let's close the db
-            res.render("index",{
-                pageId:'delay2',
-            });
-            db.close();
-        });
-    });
-});
 
 // routes ======================================================================
 require('./app/routes.js')(app, passport); // load our routes and pass in our app and fully configured passport
