66f0f935dc9a2694c8100ab79fc9062a103a49ba
samchungy <samchungy@gmail.com>
2017-05-17 23:08:57 +1000

    Now takes Username as the key for the new user. Added server side validation for signup. Added feedback messages.

--- config/passport.js
+++ config/passport.js
@@ -34,42 +34,50 @@ module.exports = function(passport) {
 	// by default, if there was no name, it would just be called 'local'
 
     passport.use('local-signup', new LocalStrategy({
-        // by default, local strategy uses username and password, we will override with email
-        usernameField : 'email',
+        // by default, local strategy uses username and password
+        usernameField : 'username',
         passwordField : 'password',
         passReqToCallback : true // allows us to pass back the entire request to the callback
     },
-    function(req, email, password, done) {
+    function(req, username, password, done) {
 
 		// find a user whose email is the same as the forms email
-        if (password !== req.param('confirm')){
-          return done(null, false, req.flash('signupMessage', 'Your passwords do not match.'));
+        if (!validateEmail(req.param('email'))){
+            return done(null, false, req.flash('signupMessage', 'Please enter a valid email address.'))
         }
+        if (!validateName(username)){
+            return done(null, false, req.flash('signupMessage', 'Please enter a valid username.'))
+        }
+        if (!validatePass(password, req.param('confirm'))){
+          return done(null, false, req.flash('signupMessage', 'Your password is invalid or does not match'));
+        }
+
 		// we are checking to see if the user trying to login already exists
-        User.findOne({ 'local.email' :  email }, function(err, user) {
+        User.findOne({ 'local.username' :  username }, function(err, user) {
             // if there are any errors, return the error
             if (err)
                 return done(err);
 
-            // check to see if theres already a user with that email
+            // check to see if theres already a user with that username
             if (user) {
-                return done(null, false, req.flash('signupMessage', 'That email is already taken.'));
-            } else {
+                return done(null, false, req.flash('signupMessage', 'That username is already taken.'));
+            }
+                else {
 
 				// if there is no user with that email
                 // create the user
                 var newUser            = new User();
 
                 // set the user's local credentials
-                newUser.local.email    = email;
-                newUser.local.username = req.param('username');
+                newUser.local.email = req.param('email');
+                newUser.local.username = username;
                 newUser.local.password = newUser.generateHash(password); // use the generateHash function in our user model
 
 				// save the user
                 newUser.save(function(err) {
                     if (err)
                         throw err;
-                    return done(null, newUser);
+                    return done(null, newUser, req.flash('successRegister', 'You have successfully registered. Welcome to UnDelay!'));
                 });
             }
 
@@ -85,15 +93,14 @@ module.exports = function(passport) {
 
     passport.use('local-login', new LocalStrategy({
         // by default, local strategy uses username and password, we will override with email
-        usernameField : 'email',
+        usernameField : 'username',
         passwordField : 'password',
         passReqToCallback : true // allows us to pass back the entire request to the callback
     },
-    function(req, email, password, done) { // callback with email and password from our form
+    function(req, username, password, done) { // callback with username and password from our form
 
-        // find a user whose email is the same as the forms email
         // we are checking to see if the user trying to login already exists
-        User.findOne({ 'local.email' :  email }, function(err, user) {
+        User.findOne({ 'local.username' :  username }, function(err, user) {
             // if there are any errors, return the error before anything else
             if (err)
                 return done(err);
@@ -107,9 +114,22 @@ module.exports = function(passport) {
                 return done(null, false, req.flash('loginMessage', 'Oops! Wrong password.')); // create the loginMessage and save it to session as flashdata
 
             // all is well, return successful user
-            return done(null, user);
+            return done(null, user, req.flash('successRegister', 'Welcome Back to UnDelay!'));
         });
 
     }));
+    function validateEmail(email) {
+      var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+      return re.test(email);
+    }
 
+    function validateName(username) {
+        var re = /(?=^.{3,20}$)^[a-zA-Z][a-zA-Z0-9]*[._-]?[a-zA-Z0-9]+$/;
+        return re.test(username);
+    }
+
+    function validatePass(p1, p2){
+        var re = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/;
+        return (re.test(p1) && p1==p2);
+    }
 };
