78f7414477b58e97ebad1b51154051c7e6db3387
Paul Hellsten <paulthegreen@live.com>
2017-05-18 09:41:18 +1000

    Revert "Revert "Revert "Update mainmap.js"""
    This reverts commit ffddb1f3c558d76c64c91b07387af5dc278960ac.

--- public/js/mainmap.js
+++ public/js/mainmap.js
@@ -43,245 +43,41 @@ $(document).ready(function () {
         // return $.getJSON("map/geojson/"+aid+"/"+rid);
     }
 
-
-	function getDelays(lid) {
-		$.ajax({
-			url: "/map/delays/",
-			type: "GET",
-			dataType: "json",
-			contentType: "application/json",
-			timeout: 5000,
-			success: function(data) {
-				addDelays(JSON.stringify(data),lid);
-			},
-		    failure: function(err) {
-			   console.log(err);
-		    }
-
-		});
-	}
-
-	function addDelays(json,lid) {
-		var data = JSON.parse(json);
-		// console.log('delays '+data);
-		for (var i=0; i<data.length; i++) {
-            var route = data[i]['route'];
-            if (route === routeMapper['routeMapper']) {
-    			var coords = data[i]['locationData'];
-    			// console.log(data[i]);
-    			if (coords == "default") { continue; }
-    			var coords_split = coords.split(',');
-    			var lat = parseFloat(coords_split[0]);
-    			var lon = parseFloat(coords_split[1]);
-    			var marker = L.marker([lat, lon]).addTo(map);
-                var type = parseInt(data[i]['type']);
-                var dtype = "Invalid type";
-                if (type === 0) {
-                    dtype = "Minor Delay";
-                } else if (type === 1) {
-                    dtype = "Major Delay";
-                } else if (type === 2) {
-                    dtype = "Cancellation of Service";
-                } else if (type === 3) {
-                    dtype = data[i]['delayType'];
-                }
-    			marker.bindPopup("<b>Delay</b><br>Delay Type: "
-    								+dtype+"<br>Details: "+data[i]['description']);
-                if (lid in mappedRoutes) {
-                    mappedRoutes[lid][1].push(marker);
-                } else {
-                    mappedRoutes[lid] = [null, [marker]];
-                }
+    function getDelays() {
+        $.ajax({
+            url: "/map/delays/",
+            type: "GET",
+            dataType: "json",
+            contentType: "application/json",
+            timeout: 5000,
+            success: function (data) {
+                addDelays(JSON.stringify(data));
+            },
+            failure: function (err) {
+                console.log(err);
             }
-		}
-	}
 
-	function getRouteNames(callback) {
-		$.ajax({
-			url: "/map/route_names/0",
-			type: "GET",
-			dataType: "json",
-			contentType: "application/json",
-			cache: false,
-			timeout: 5000,
-			success: function(data) {
-			  callback(0,data);
-		   }
-		});
-		$.ajax({
-			url: "/map/route_names/1",
-			type: "GET",
-			dataType: "json",
-			contentType: "application/json",
-			cache: false,
-			timeout: 5000,
-			success: function(data) {
-			  callback(1,data);
-		   }
-		});
-		$.ajax({
-			url: "/map/route_names/2",
-			type: "GET",
-			dataType: "json",
-			contentType: "application/json",
-			cache: false,
-			timeout: 5000,
-			success: function(data) {
-			  callback(2,data);
-		   }
-		});
-	}
+        });
+    }
 
-    // Map route ids to full route name
-    var routeMapper = {};
+    getDelays();
 
-	function populateRouteList(aid,rarr) {
-		// console.log(aid+' '+rarr);
-		// for (key in rdict) { console.log(key); }
-		if (!rarr) { return; }
-
-		switch(aid){
-			// Metro Buses
-			case 0:
-			for (var i=0; i<rarr.length; i++) {
-				$("#metrobuslist").append('<li class="list-group-item"><label><input type="checkbox" class="rbox" id=cbox0'+i+'>'+rarr[i]+"</label></li>");
-				$("#cbox0"+i).change(function() {
-					// if checked
-					if ($(this).is(':checked')) {
-						addRouteLayer($(this));
-					} // if unchecked
-					else {
-						removeRouteLayer($(this));
-					}
-				});
-                routeMapper[rarr[i]] = '0'+i.toString();
-			}
-			break;
-			// Metro Trains
-			case 1:
-			for (var i=0; i<rarr.length; i++) {
-				$("#metrotrainlist").append('<li class="list-group-item"><label><input type="checkbox" class="rbox" id=cbox1'+i+'>'+rarr[i]+"</label></li>");
-				$("#cbox1"+i).change(function() {
-					// if checked
-					if ($(this).is(':checked')) {
-						addRouteLayer($(this));
-					} // if unchecked
-					else {
-						removeRouteLayer($(this));
-					}
-				});
-                routeMapper['1'+i.toString()] = rarr[i];
-			}
-			break;
-			// Metro Trams
-			case 2:
-			for (var i=0; i<rarr.length; i++) {
-				$("#metrotramlist").append('<li class="list-group-item"><label><input type="checkbox" class="rbox" id=cbox2'+i+'>'+rarr[i]+"</label></li>");
-				$("#cbox2"+i).change(function() {
-					// if checked
-					if ($(this).is(':checked')) {
-						addRouteLayer($(this));
-					} // if unchecked
-					else {
-						removeRouteLayer($(this));
-					}
-				});
-                routeMapper['2'+i.toString()] = rarr[i];
-			}
-			break;
-		}
-	}
-
-	var mappedRoutes = {};
-
-	function addRouteLayer(cbox) {
-		cid = cbox.attr('id');
-		// console.log(cid);
-		agency = parseInt(cid.substring(4,5));
-		route = parseInt(cid.substring(5));
-		getGeoJSONLayer(agency,route,addMapLayer);
-		lid = agency.toString + route.toString;
-        mappedRoutes[lid] = [null, []];
-        getDelays(lid);
-	}
-
-	function addMapLayer(aid,rid,mapdata) {
-		layerstyle = null;
-		if (aid == 0) { layerstyle = defBusStyle }
-		else if (aid == 1) {
-            layerstyle = defTrainStyle;
-            if (mapdata['colour']) {
-                layerstyle.color = mapdata['colour'];
+    function addDelays(json) {
+        var data = JSON.parse(json);
+        // console.log('delays '+data);
+        for (var i = 0; i < data.length; i++) {
+            var coords = data[i]['locationData'];
+            // console.log(data[i]);
+            if (coords == "default") {
+                continue;
             }
-		}
-		else if (aid == 2) {
-			layerstyle = defTramStyle;
-            if (mapdata['colour']) {
-                layerstyle.color = mapdata['colour'];
-            }
-		}
-		newlayer = L.geoJson(mapdata, {
-			style: layerstyle,
-			onEachFeature: onEachRoute
-		});
-		lid = aid.toString() + rid.toString();
-		// console.log('add '+lid);
-		newlayer.addTo(map);
-        if (lid in mappedRoutes) {
-            mappedRoutes[lid][0] = newlayer;
-        } else {
-            mappedRoutes[lid] = [newlayer, []];
+            var coords_split = coords.split(',');
+            var lat = parseFloat(coords_split[0]);
+            var lon = parseFloat(coords_split[1]);
+            var marker = L.marker([lat, lon]).addTo(map);
+            marker.bindPopup("<b>Delay</b><br>Location: " + data[i]['location'] + "<br>Delay Type: "
+                + data[i]['DelayType'] + "<br>Details: " + data[i]['description']);
         }
-
-	}
-
-	function removeRouteLayer(cbox) {
-		lid = cbox.attr('id').substring(4);
-		// console.log('rem '+lid);
-		if (lid in mappedRoutes) {
-            if (mappedRoutes[lid][0]) {
-                map.removeLayer(mappedRoutes[lid][0]);
-    			delete mappedRoutes[lid][0];
-            }
-            if (mappedRoutes[lid][1]) {
-                for (var i=0; i<mappedRoutes[lid][1]; i++){
-                    map.removeLayer(mappedRoutes[lid][1][i]);
-        			delete mappedRoutes[lid][1][i];
-                }
-                delete mappedRoutes[lid][1];
-            }
-            delete mappedRoutes[lid];
-
-		}
-	}
-
-	// $.when(getRouteNames()).done(function(d1) {
-		// console.log('d1 '+d1[1]);
-		// populateRouteList(d1[1]);
-	// });
-
-	getRouteNames(populateRouteList);
-
-/*     function populateFilterList(type, geodata) {
-        switch (type) {
-            case "tram":
-                var routenames = [];
-                for (var i = 0; i < geodata.features.length; i++) {
-                    routenames.push(geodata.features[i].properties["LINEDESCLG"]);
-                };
-                var uniqueroutenames = [];
-                $.each(routenames, function(i, el) {
-                    if ($.inArray(el, uniqueroutenames) === -1) {
-                        uniqueroutenames.push(el);
-                        $("#tramlist").append('<li class="list-group-item">' + el + "</li>");
-                    };
-                });
-                break;
-            case "train":
-                // for (var i = 0; i < geodata.features.length; i++) {
-                //     $("#trainlist").append('<li class="list-group-item">' + geodata.features[i].properties["LINEDESCLG"] + "</li>");
-                // };
-=======
     }
 
     function getRouteNames(callback) {
@@ -342,7 +138,6 @@ $(document).ready(function () {
                         }
                     });
                 }
->>>>>>> origin/master
                 break;
             // Metro Trains
             case 1:
