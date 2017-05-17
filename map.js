63ef4b9c7846a3e6c047080ce413f660d2e00cb8
Cameron Dempsey <cameron.dempsey@gmail.com>
2017-05-18 09:06:32 +1000

    Update map.js

--- map.js
+++ map.js
@@ -56,12 +56,15 @@ router.get('/route_names/:agency([0-2])',function(req,res){
 	else if (agency == 2) { agency_name = 'PTV-MetroTram'; }
 	Route.find({agency_key: agency_name},'route_short_name route_long_name agency_name', function(error,ids) {
 		rlist = []
+		namechecklist = [];
 		for (var i=0; i<ids.length; i++) {
+			if (namechecklist.includes(ids[i]['route_short_name'])) { continue; }
 			if (ids[i]['agency_name'] == 'PTV-MetroTrain') {
 				rlist.push(ids[i]['route_short_name']);
 			} else {
 				rlist.push(ids[i]['route_short_name']+' - '+ids[i]['route_long_name']);
 			}
+			namechecklist.push(ids[i]['route_short_name']);
 		}
 		rlist.sort();
 		res.contentType('json');
